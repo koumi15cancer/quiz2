1f863da43f50663864b2ae1e4e230700c1132276
koumi15cancer <s3757278@rmit.edu.vn>
2021-09-17 00:49:29 +0700

    add plus function

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ app.get("/new", function (req, res) {
   res.send("New also worked");
 });
 
+app.get('/add', function(req, res) {
+    var a = Number(req.query.a);
+    var b = Number(req.query.b)
+    if (!isNaN(a) && !isNaN(b)) {
+        res.send((a + b).toString())
+    } else {
+        res.send("a, b must be number")
+    }
+});
+
 app.listen(process.env.PORT || 5000);
 module.exports = app;
