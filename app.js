ec0e913c812e16c3181131e8922d2c911c9c2138
koumi15cancer <s3757278@rmit.edu.vn>
2021-09-17 09:22:34 +0700

    assignment

--- app.js
+++ app.js
@@ -8,15 +8,33 @@ app.get("/new", function (req, res) {
   res.send("New also worked");
 });
 
-app.get('/add', function(req, res) {
-    var a = Number(req.query.a);
-    var b = Number(req.query.b)
-    if (!isNaN(a) && !isNaN(b)) {
-        res.send((a + b).toString())
-    } else {
-        res.send("a, b must be number")
-    }
+app.get('/add', function (req, res) {
+  var a = Number(req.query.a);
+  var b = Number(req.query.b)
+  if (!isNaN(a) && !isNaN(b)) {
+    res.send((a + b).toString())
+  } else {
+    res.send("a, b must be number")
+  }
 });
+
+app.get('/process', function (req, res) {
+  var input = String(req.query.a);
+
+  if (input !== null) {
+    input.replace("\\s+", " ").trim()
+    const output = input.split(" ");
+    for (var i = 0; i < output.length; i++) {
+      output[i] = output[i].charAt(0).toUpperCase() + output[i].slice(1);
+
+    }
+    const result = output.join(" ").toString();
+    res.send(result)
+  } else {
+    res.send("error")
+  }
+});
+
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
